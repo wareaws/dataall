391b05ed04199520ad95f31c05c743b3681a9a20
wareaws <129090374+wareaws@users.noreply.github.com>
2025-09-19 15:05:48 -0400

    Update runtime_options.py

--- deploy/stacks/runtime_options.py
+++ deploy/stacks/runtime_options.py
@@ -1,1 +1,13 @@
-../../backend/dataall/runtime_options.py
+python
+from aws_cdk.aws_lambda import Runtime
+
+"""
+Changing this will change the python runtime in:
+* PROD Dockerfiles (lambda and ecs)
+* Lambda runtimes
+Will not change (so must manually be changed) in:
+* dev/Dockerfile
+"""
+PYTHON_VERSION = '3.12'
+
+PYTHON_LAMBDA_RUNTIME = getattr(Runtime, f'PYTHON_{PYTHON_VERSION.replace(".", "_")}')
